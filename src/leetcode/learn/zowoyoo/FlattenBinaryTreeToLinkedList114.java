ff2cff89319ec8414c8775515f4f14053fc8419c
KamTo <478409784@qq.com>
2020-12-03 18:41:44 +0800

    算法练习

--- src/leetcode/learn/zowoyoo/FlattenBinaryTreeToLinkedList114.java
+++ src/leetcode/learn/zowoyoo/FlattenBinaryTreeToLinkedList114.java
@@ -8,4 +8,34 @@ package leetcode.learn.zowoyoo;
  * @since 2020/12/3 17:23
  */
 public class FlattenBinaryTreeToLinkedList114 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode leftNode = root.left;
+        TreeNode rightNode = root.right;
+        root.left = null;
+        root.right = leftNode;
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = rightNode;
+    }
 }
