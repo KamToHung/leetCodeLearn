dd3171bd407b4b4e7cf2d8b7085ea3ff90812cea
KamTo <478409784@qq.com>
2020-12-03 17:24:04 +0800

    算法练习

---
+++ src/leetcode/learn/zowoyoo/FlattenBinaryTreeToLinkedList114.java
@@ -0,0 +1,11 @@
+package leetcode.learn.zowoyoo;
+
+/**
+ * 114
+ * 给定一个二叉树，原地将它展开为一个单链表
+ *
+ * @author Terry
+ * @since 2020/12/3 17:23
+ */
+public class FlattenBinaryTreeToLinkedList114 {
+}
---
+++ src/leetcode/learn/zowoyoo/PopulatingNextRightPointersInEachNode116.java
@@ -0,0 +1,48 @@
+package leetcode.learn.zowoyoo;
+
+/**
+ * TODO
+ *
+ * @author Terry
+ * @since 2020/12/3 17:06
+ */
+public class PopulatingNextRightPointersInEachNode116 {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        connectTwoNode(root.left,root.right);
+        return root;
+    }
+
+    private void connectTwoNode(Node leftNode,Node rightNode){
+        if (leftNode == null || rightNode == null) {
+            return;
+        }
+        leftNode.next = rightNode;
+        connectTwoNode(leftNode.left,leftNode.right);
+        connectTwoNode(rightNode.left,rightNode.right);
+        connectTwoNode(leftNode.right,rightNode.left);
+    }
+
+}
