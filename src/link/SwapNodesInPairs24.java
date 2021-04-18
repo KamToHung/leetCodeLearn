35ba124063d199d9f2be39b55208b94b9dee4773
Terry <478409784@qq.com>
2021-04-18 11:37:47 +0800

    leetcode24

---
+++ src/link/SwapNodesInPairs24.java
@@ -0,0 +1,51 @@
+package link;
+
+/**
+ * //给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = [1]
+ * 输出：[1]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ * <p>
+ * <p>
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ */
+public class SwapNodesInPairs24 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+}
